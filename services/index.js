8a71125eecab8eb9510f92ed0ed7827efc8957b8
MD. MOJNU MIAH <94952925+MojnuCSE@users.noreply.github.com>
2022-11-19 00:08:07 +0600

    first

--- services/index.js
+++ services/index.js
@@ -184,7 +184,7 @@ export const getCategoryPost = async (slug) => {
 export const getFeaturedPosts = async () => {
   const query = gql`
     query GetCategoryPost() {
-      posts(where: {featuredPost: true}) {
+      posts(where: {featurdPost: true}) {
         author {
           name
           photo {
